e8fbed7eb7f57885e78561bcc6a229f835a9a125
terencecistudent <terencelogue100@hotmail.co.uk>
2020-03-21 19:31:29 +0000

    Working on password reset

--- django_auth_tl/urls.py
+++ django_auth_tl/urls.py
@@ -15,13 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from accounts.views import index, logout, login, registration, user_profile
+from accounts.views import index
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', index, name="index"),
-    url(r'^accounts/logout/$', logout, name="logout"),
-    url(r'^accounts/login/$', login, name="login"),
-    url(r'^accounts/register/$', registration, name="registration"),
-    url(r'^accounts/profile/$', user_profile, name="profile")
+    url(r'^$', index, name="index")
 ]
